80291d71837a938a0064cee3cbd38425da0227a7
jubhavsar <juhi_bhavsar@yahoo.com>
2022-05-05 23:37:23 -0400

    implement paintToCanvas function with same height and width

--- 19-Webcam-Fun/scripts.js
+++ 19-Webcam-Fun/scripts.js
@@ -18,4 +18,17 @@ function getVideo() {
 
 }
 
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  console.log(height, width);
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 16);
+}
+
 getVideo();
+
